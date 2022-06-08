ce68a97fabe1e198ebe95075cf75e9d810862995
danny galimi <dannygalimi13@aim.com>
2022-06-08 13:15:12 -0400

    commit tree df95a8207dc0026e0e6b761ddf5d35c9fc650dff

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,17 @@
+
+   
+class MP3Importer
+    attr_reader :path
+  
+    def initialize(path)
+      @path = path
+    end
+  
+    def files
+      @files 
+    end
+  
+    def import
+      files.each{|f| Song.new_by_filename(f)}
+    end
+  end
